c1665138feb25d1be3a655d528d47b20cfc08845
Volodymyr Yepishev <i.m.bwca@gmail.com>
2022-06-11 21:57:19 +0300

    init commit, empty maven project

---
+++ src/main/java/cursorBot/App.java
@@ -0,0 +1,16 @@
+package cursorbot;
+
+import java.awt.Robot;
+import java.util.Random;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/cursorBot/AppTest.java
@@ -0,0 +1,20 @@
+package cursorbot;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
