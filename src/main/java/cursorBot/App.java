d2794cd84f696687df3dccd9af1b726ab8230146
Volodymyr Yepishev <i.m.bwca@gmail.com>
2022-06-12 09:03:41 +0300

    added logic for moving the cursor

--- src/main/java/cursorBot/App.java
+++ src/main/java/cursorBot/App.java
@@ -1,16 +1,36 @@
 package cursorbot;
 
 import java.awt.Robot;
-import java.util.Random;
+import java.awt.AWTException;
+import java.awt.MouseInfo;
+
 
 /**
- * Hello world!
- *
+ * Cursor mover!
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String... args) {
+        moveMouse();
+    }
+
+    private static void moveMouse() {
+        try {
+            final Robot robot = new Robot();
+            int mouseX = (int) MouseInfo.getPointerInfo().getLocation().getX();
+            final int mouseY = (int) MouseInfo.getPointerInfo().getLocation().getY();
+            final byte step = 100;
+            byte multiplier = 1;
+            final short sleepTime = 15000;
+
+            while (true) {
+                int x = mouseX + step * multiplier;
+                multiplier *= -1;
+                robot.mouseMove(x, mouseY);
+                Thread.sleep(sleepTime);
+            }
+        } catch (AWTException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
